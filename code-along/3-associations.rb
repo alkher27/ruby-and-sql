e253d915f6089a5337ce787341dc7cdc1de69dbc
alkher27 <alikheraj@gmail.com>
2023-05-07 14:45:23 +0000

    third commit

--- code-along/3-associations.rb
+++ code-along/3-associations.rb
@@ -11,7 +11,30 @@ Contact.destroy_all
 # - Insert and read contact data for companies in the database
 
 # 1. insert new rows in the contacts table with relationship to a company
+apple = Company.find_by({ "name" => "Apple" })
+
+contact = Contact.new
+contact["first_name"] = "Tim"
+contact["last_name"] = "Cook"
+contact["email"] = "tim@apple.com"
+contact["company_id"] = apple["id"]
+contact.save
+
+apple = Company.find_by({ "name" => "Apple" })
+
+contact = Contact.new
+contact["first_name"] = "Craig"
+contact["last_name"] = "Federighi"
+contact["email"] = "craig@apple.com"
+contact["company_id"] = apple["id"]
+contact.save
+
 
 # 2. How many contacts work at Apple?
+apple_contacts = Contact.where({"company_id" => apple["id"]})
+puts "Contacts at Apple: #{apple_contacts.count}"
 
 # 3. What is the full name of each contact who works at Apple?
+for contact in apple_contacts
+    puts "#{contact["first_name"]} #{contact["last_name"]}"
+end
